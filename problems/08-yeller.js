51b83af0db8480d7a72058ea25b227f7047d1764
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-07-01 16:36:04 -0400

    08-yeller.js done

--- problems/08-yeller.js
+++ problems/08-yeller.js
@@ -3,13 +3,19 @@ Define a function yeller that takes in an array of words. The function should
 return a NEW array where each element of the original array is yelled.
 */
 
-// Your code here 
+// Your code here
+function yeller(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].toUpperCase();
+    }
+    return arr;
+}
 
 
-// let words1 = ['hello', 'world'];
-// let yelled1 = yeller(['hello', 'world']);
-// console.log(yelled1);              //=> [ 'HELLO!', 'WORLD!' ]
-// console.log(words1 === yelled1);   //=> false
+let words1 = ['hello', 'world'];
+let yelled1 = yeller(['hello', 'world']);
+console.log(yelled1); //=> [ 'HELLO!', 'WORLD!' ]
+console.log(words1 === yelled1); //=> false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = yeller;
