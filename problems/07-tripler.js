fecfc10d87482572ce4ebc6034970f756e192e6d
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-07-01 16:28:45 -0400

    07-tripler.js done

--- problems/07-tripler.js
+++ problems/07-tripler.js
@@ -4,11 +4,17 @@ should return a new array containing three times every number of the original
 array.
 */
 
-// Your code here 
+// Your code here
+function tripler(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i] * 3;
+    }
+    return arr;
+}
 
 
-// console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
-// console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
+console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
+console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = tripler;
